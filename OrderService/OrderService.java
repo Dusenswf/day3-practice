105e69e0992610116a9817598e59dbc39d16cc88
Dusenswf <wkdduswn7338@naver.com>
2026-09-16 10:12:24 +0900

    refactor: OrderService(변수, Loop 쪼개기, 문장 이동 등)

--- OrderService/OrderService.java
+++ OrderService/OrderService.java
@@ -42,32 +42,50 @@ class Order {
 public class OrderService {
 
     public void processOrder(Customer customer, List<Order> orders) {
-        double totalAmount = 0;
-        String shippingLabels = "";
-        int totalPoints = 0;
 
+        // 가격 계산
+        double orderTotalPrice = 0;
         for (Order order : orders) {
             double currentPrice = order.getPrice() * order.getQuantity();
-
             if (order.isEarlyDelivery()) {
-                totalAmount += currentPrice + 5000;
-                totalPoints += (int) (currentPrice * 0.05);
+                orderTotalPrice += currentPrice + 5000;
+            } else {
+                orderTotalPrice += currentPrice;
+            }
+        }
+
+        // 라벨링
+        String shippingLabels = "";
+        for (Order order : orders) {
+            if (order.isEarlyDelivery()) {
                 shippingLabels += "[새벽]" + order.getId() + " " + customer.getAddress() + ", ";
             } else {
-                totalAmount += currentPrice;
-                totalPoints += (int) (currentPrice * 0.01);
                 shippingLabels += "[일반]" + order.getId() + " " + customer.getAddress() + ", ";
             }
         }
 
-        if (customer.getType().equals("VIP")) {
-            totalAmount = totalAmount * 0.9;
-            totalPoints += 500;
+        // 포인트 계산
+        int earnedPoints = 0;
+        for (Order order : orders) {
+            double currentPrice = order.getPrice() * order.getQuantity();
+            if (order.isEarlyDelivery()) {
+                earnedPoints += (int) (currentPrice * 0.05);
+            } else {
+                earnedPoints += (int) (currentPrice * 0.01);
+            }
         }
 
-        System.out.println("적립 포인트: " + totalPoints);
+
+        double finalAmount = 0;
+        int finalPoints = 0;
+        if (customer.getType().equals("VIP")) {
+            finalAmount = orderTotalPrice * 0.9;
+            finalPoints = earnedPoints + 500;
+        }
+
+        System.out.println("적립 포인트: " + finalPoints);
         System.out.println("배송지 목록: " + shippingLabels);
-        System.out.println("최종 결제 금액: " + totalAmount);
+        System.out.println("최종 결제 금액: " + finalAmount);
     }
 }
 
